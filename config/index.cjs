07c669ae362c20c448f137bfb2ff1e7b5d27986b
MagniiCheng <57683851+MagniiCheng@users.noreply.github.com>
2023-04-10 09:32:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21581TTIQHbPCFhzRE2e3xZeqRaRqsjRN7l3Uo',
+      id: 'o6cKL6FIJP03OZ-36s2tySr79l88',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -53,13 +53,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'riZhUtsW-xh5rfIJncp23M5xH8dwuRguYC7Uz9PwNI0',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'magniicheng',
+      id: 'o6cKL6FIJP03OZ-36s2tySr79l88',
     }
   ],
 
