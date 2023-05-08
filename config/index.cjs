99dbc8a36263b6a7861bd29be0e343fcb4c9d7c0
MagniiCheng <57683851+MagniiCheng@users.noreply.github.com>
2023-05-08 14:08:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6cKL6FIJP03OZ-36s2tySr79l88',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'saLkHojr-LgyvHpJPmfZvBYOwAg9I8_gN4-NASECJ4w',
+      useTemplateId: 'J97-wIW9TVvbeMGeEKGuI2dyZCHFDx_Y-GAim_KmWAw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-02',
       festivals: [
