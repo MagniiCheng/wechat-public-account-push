1007e0d8b327da46b17a353a7237740e9167061a
MagniiCheng <57683851+MagniiCheng@users.noreply.github.com>
2023-05-08 14:31:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '加盐宝贝🧂',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o6cKL6FIJP03OZ-36s2tySr79l88',
+      id: ["o6cKL6FIJP03OZ-36s2tySr79l88","o6cKL6PGajDIyARSbFPz8F3HoRI4"],
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'NZKEZkPa5O8FYgyI1ZdkJ202Qgq0dnCv5hum3jl6Muo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
